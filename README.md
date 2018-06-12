6880e8ee9be05f0d74c44475998954af79be2cf0
win7 <737141437@qq.com>
2018-06-12 18:39:05 +0800

    1、一些功能上的代码的提交 2.提交一些实例的代码 3、demo的提交

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # SmallProgram
 微信小程序，学习使用（WeChat）
 *   建议熟悉了，可以多看看这个Demo**甜果果小铺商城案例（微信小程序）** https://github.com/Shimingli/TianguoguoXiaopu 写的比较好 
+*  记得每次提交下，要不然，没有同步就尴尬了
--- pages/index/index.js
+++ pages/index/index.js
@@ -20,7 +20,7 @@ const app = getApp()
 Page({
   // 页面的初始数据
   data: {
-    motto: 'ddd',
+    motto: 'Demo start',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
@@ -33,39 +33,83 @@ Page({
     })
   },
   onUnload :function(){
-    console.log("shiming 首页在页面卸载了")
+    console.log("shiming index onUnload")
   },
   onHide :function(){
-    console.log("shiming 首页在隐藏了")
+    console.log("shiming index onHide ")
   },
   onShow : function(){
-    console.log("shiming 首页的页面显示了")
+    console.log("shiming index onShow")
   },
   onReady: function(){
-    console.log("shiming 首页在准备了")
+    console.log("shiming  index onReady")
   },
 //   onPullDownRefresh   
 // 监听用户下拉动
 // 1）需要在config的window选项中开启enablePullDownRefresh。
 // 2）当处理完数据刷新后，wx.stopPullDownRefresh 可以停止当前页面的下拉刷新。
   onPullDownRefresh : function(){
-    console.log("shiming 首页在下拉刷新了")
+    console.log("shiming  index onPullDownRefresh")
     wx.stopPullDownRefresh
   },
   //onReachBottom   
   // 页面上拉触底事件的处理函数
+  /**
+   * 和 onPullDownRefresh 事件相对于，只有onPullDownRefresh的事件触发了，这个事件才会触发
+   * 页面上拉触底事件的处理函数
+   */
   onReachBottom : function(){
-    console.log("shiming 首页在拉到底部了")
+    console.log("shiming index onReachBottom")
+  },
+  /**
+   * 	生命周期函数--监听页面卸载 好像废弃掉了
+   */
+  onUnLoad :function(){
+    console.log("shiming index onUnLoad")
+  },
+  /**
+   * onShareAppMessage  用户点击右上角转发
+   */
+  onShareAppMessage :function(){
+     console.log("shiming index onShareAppMessage");
+  },
+  /**
+   * 代码没有提醒出这个方法是否的有效，但是呢，实际上这个方法是有效果的哦
+   * 页面滚动触发事件的处理函数 onPageScroll
+   */
+  onPageScroll : function(){
+     console.log("shiming index onPageScroll")
+  },
+  /**
+   * onTabItemTap 这个方法，就好像多次的点击了下面的Tab的时候，都会执行
+   * 这个方法也提醒不出来，可能是我的 设置的问题，所以还要看看 到底是什么的问题
+   * 这个方法也是管作用的 
+   * 我在想能不能 调用里面的方法 
+   * 如果下面的方法注释掉的话，会调用这个方法
+   */
+  onTabItemTap :function(){
+    console.log("shiming index onTabItemTap");
+  },
+  /**
+   * 如果不注释，多次的点击看看,只有第二次的点击才会走这个方法 
+   */
+  onTabItemTap(item) {
+    console.log("shiming index item.index=="+item.index)
+    console.log("shiming index item.pagePath=="+item.pagePath)
+    console.log("shiming index item.text=="+item.text)
+  },
+  customData: {
+    hi: 'MINA'
   },
   onLoad: function () {
-    console.log("shiming 首页开始加载了")
+    console.log("shiming index onLoad")
     var logs = wx.getStorageSync('logs') || []
     logs.unshift("首页在加载了")
     wx.setStorageSync('logs', logs)
-    console.log("shiming 首页开始加载了 app.globalData.userInfo==" + app.globalData.userInfo)
-    console.log("shiming 首页开始加载了  this.data.canIUse==" + this.data.canIUse)
+    console.log("shiming index onLoad app.globalData.userInfo==" + app.globalData.userInfo)
+    console.log("shiming index  onLoad  this.data.canIUse==" + this.data.canIUse)
     if (app.globalData.userInfo) {
-      console.log("shiming 首页开始加载了" + app.globalData.userInfo)
+      console.log("shiming index onLoad" + app.globalData.userInfo)
       this.setData({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
@@ -74,8 +118,8 @@ Page({
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
-        console.log("shiming 首页开始加载了 res.userInfo==" + res.userInfo.avatarUrl)
-        console.log("shiming 首页开始加载了 res.nickName==" + res.userInfo.nickName)
+        console.log("shiming index onLoad res.userInfo==" + res.userInfo.avatarUrl)
+        console.log("shiming index onLoad res.nickName==" + res.userInfo.nickName)
         this.setData({
           userInfo: res.userInfo,
           hasUserInfo: true
