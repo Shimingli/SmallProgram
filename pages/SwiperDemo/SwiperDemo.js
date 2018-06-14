08c08fd422b9c268a3b02cd9572b695ba621cfee
win7 <737141437@qq.com>
2018-06-14 11:57:06 +0800

    1、提交的完整的Swiper的代码 2、还有另外一个Demo 3、同时注释了一些Slider的用法

--- pages/SwiperDemo/SwiperDemo.js
+++ pages/SwiperDemo/SwiperDemo.js
@@ -4,16 +4,39 @@ Page({
     indicatorDots: true,//indicator-dots	Boolean	false	是否显示面板指示点
     vertical: false,//vertical	Boolean	false	滑动方向是否为纵向
     autoplay: true,//autoplay	Boolean	false	是否自动切换
-    circular: false,//circular	Boolean	false	是否采用衔接滑动 就是滑动完了，在继续的滑动，而不是滑动到首页 
+    circular: true,//circular	Boolean	false	是否采用衔接滑动 就是滑动完了，在继续的滑动，而不是滑动到首页 
     interval: 2000,//interval	Number	2000	自动切换时间间隔
     duration: 500,//duration	Number	500	滑动动画时长
     previousMargin: 0,//previous-margin	String	"0px"	前边距，可用于露出前一项的一小部分，接受 px 和 rpx 值
-    nextMargin: 0//next-margin	String	"0px"	后边距，可用于露出后一项的一小部分，接受 px 和 rpx 值
+    nextMargin: 0,//next-margin	String	"0px"	后边距，可用于露出后一项的一小部分，接受 px 和 rpx 值
+    swiperCurrent:0
   },
+  swipclick : function(){
+    console.log("shiming swipclick" + this.data.swiperCurrent)
+    if (this.data.swiperCurrent==0){
+      wx.navigateTo({
+        url: '/pages/SwiperDemo/SwiperDemoAnather',
+      })
+    }
+  },
+  //轮播图的切换事件
+  swiperChange: function (e) {
+    console.log("shiming  wiperChange" + e.detail.current)
+    this.setData({
+      swiperCurrent: e.detail.current
+    })
+  },
+  //: switch类型切换时在iOS自带振动反馈，可在系统设置 -> 声音与触感 -> 系统触感反馈中关闭
   changeProperty: function (e) {
+    //改变里面的对象，这里很好的例子 
     var propertyName = e.currentTarget.dataset.propertyName
     var newData = {}
+    //perpertyNameindicatorDots
+    console.log("shiming perpertyName" + propertyName)
     newData[propertyName] = e.detail.value
+    //e.detail.valuetrue
+    console.log("shiming e.detail.value" + e.detail.value)
+    console.log("shiming newData" + newData.toString)
     this.setData(newData)
   },
   changeIndicatorDots: function (e) {
---
+++ pages/SwiperDemo/SwiperDemoAnather.js
@@ -0,0 +1,35 @@
+//如果在 bindchange 的事件回调函数中使用 setData 改变 current 值，则有可能导致 setData 被不停地调用，
+//因而通常情况下请在改变 current 值前检测 source 字段来判断是否是由于用户触摸引起。
+Page({
+  data: {
+    imgUrls: [
+      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+    ],
+    indicatorDots: false,
+    autoplay: false,
+    interval: 5000,
+    duration: 1000
+  },
+  changeIndicatorDots: function (e) {
+    this.setData({
+      indicatorDots: !this.data.indicatorDots
+    })
+  },
+  changeAutoplay: function (e) {
+    this.setData({
+      autoplay: !this.data.autoplay
+    })
+  },
+  intervalChange: function (e) {
+    this.setData({
+      interval: e.detail.value
+    })
+  },
+  durationChange: function (e) {
+    this.setData({
+      duration: e.detail.value
+    })
+  }
+})
