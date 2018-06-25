d68890fdf2256c5f77e6783797dd20c75f7f3a2e
win7 <737141437@qq.com>
2018-06-25 15:18:37 +0800

    1.提交地图的Demo

--- pages/MediaComponents/CameraDemo.js
+++ pages/MediaComponents/CameraDemo.js
@@ -1,3 +1,9 @@
+
+
+// tip: camera 组件是由客户端创建的原生组件，它的层级是最高的，不能通过 z- index 控制层级。可使用 cover- view cover- image覆盖在上面。
+// tip: 同一页面只能插入一个 camera 组件。
+// tip: 请勿在 scroll- view、swiper、picker - view、movable - view 中使用 camera 组件。
+// bug: scan - area 属性目前存在识别区域不准的问题，建议先不指定
 Page({
   onLoad() {
     //创建并返回 camera 上下文 cameraContext 对象，cameraContext 与页面的 camera 组件绑定，一个页面只能有一个camera，通过它可以操作对应的 <camera/> 组件。 在自定义组件下，第一个参数传入组件实例this，以操作组件内 <camera/> 组件
@@ -6,7 +12,7 @@ Page({
   //拍照去了 
   takePhoto() {
     this.ctx.takePhoto({
-      quality: 'high',
+      quality: 'high',//感觉就是质量的高低
       success: (res) => {
         console.log("shiming " + res.tempImagePath)
         this.setData({
@@ -17,7 +23,7 @@ Page({
   },
   startRecord() {
     this.ctx.startRecord({
-      success: (res) => {
+      success: (res) => {//开始录制，保存到本地 
         console.log('startRecord')
       }
     })
